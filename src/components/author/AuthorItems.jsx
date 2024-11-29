9f05a335c386b8b3c9314b69d3d24d34b9eba111
owlthinkabtit <haggins.jameka@gmail.com>
2024-11-29 11:48:26 -0500

    I have commpleted the Author component and made it so that it is dynamic. I have started on the AuthorItems component and making that dynamic also, however I am running into a problem, and this is why I am saving right now.

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -5,28 +5,31 @@ import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
 
 const AuthorItems = () => {
-  const [authorItems, setAuthorItems] = useState ([]);
+  const { id } = useParams();
+  const [author, setAuthor] = useState ([null]);
+
   useEffect(() => {
     axios 
-      .get("https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers")
+      .get( `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${id}`)
       .then((response) => {
-        setAuthorItems(response.data);
+        setAuthor(response.data);
       })
       .catch((error) => {
         console.error("Error fetching Author Items")
       });
-  }, []);
+  }, [id]);
 
+  const { nftCollection = [] } = author;
   return (
     <div className="de_tab_content">
       <div className="tab-1">
         <div className="row">
-          {authorItems.map((item, index) => (
+          {nftCollection.slice(0, 8).map((item, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft__item">
                 <div className="author_list_pp">
                   <Link to={`/author/${item.authorId}`}>
-                    <img className="lazy" src={AuthorImage} alt="" />
+                    <img className="lazy" src={item.authorImage} alt="" />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
@@ -50,20 +53,20 @@ const AuthorItems = () => {
                   </div>
                   <Link to="/item-details">
                     <img
-                      src={nftImage}
+                      src={item.nftImage}
                       className="lazy nft__item_preview"
                       alt=""
                     />
                   </Link>
                 </div>
                 <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
+                  <Link to={`/item-details/${item.id}`}>
+                    <h4>{item.title}</h4>
                   </Link>
-                  <div className="nft__item_price">2.52 ETH</div>
+                  <div className="nft__item_price">{item.price} ETH</div>
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>97</span>
+                    <span>{item.likes}</span>
                   </div>
                 </div>
               </div>
