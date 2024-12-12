65b0c5b9a1150508355cf455883cd66d9f1915fd
owlthinkabtit <haggins.jameka@gmail.com>
2024-12-12 17:48:33 -0500

    Completed the skeleton loading states for all the compoenents as best I could. It's done, however I want to use one more day to double check everything and then submit this.

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
+import Skeleton from "../UI/Skeleton";
 
 const ExploreItems = () => {
   const [exploreItems, setExploreItems] = useState([]);
   const [filter, setFilter] = useState("");
   const [page, setPage] = useState(1);
-  const [loading, setLoading] =useState(false);
+  const [loading, setLoading] = useState(false);
 
   const calculateTimeLeft = (expiryData) => {
     const timeLeft = new Date(expiryData) - new Date();
@@ -68,67 +67,97 @@ const ExploreItems = () => {
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {exploreItems.map((item, index) => (
-        <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div 
-            className="nft__item"
-            data-aos="fade-up"
-            data-aos-duration="1000">
-            <div className="author_list_pp">
-              <Link
-                to={`/author/${item.authorId}`}
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={item.authorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
+      {loading ? (
+        <div className="row">
+          {Array.from({ length: 8 }).map((_, index) => (
+            <div
+              key={index}
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ display: "block", backgroundSize: "cover" }}
+            >
+              <div className="nft__item">
+                <div className="author_list_pp">
+                  <Skeleton width="50px" height="50px" borderRadius="50%" />
+                </div>
+                <div className="de_countdown">
+                  <Skeleton width="100px" height="20px" borderRadius="4px" />
+                </div>
+                <div className="nft__item_wrap">
+                  <Skeleton width="100%" height="200px" borderRadius="8px" />
+                </div>
+                <div className="nft__item_info">
+                  <Skeleton width="70%" height="20px" borderRadius="4px" />
+                  <Skeleton width="50%" height="20px" borderRadius="4px" />
+                </div>
+              </div>
             </div>
-            <div className="de_countdown">{item.timeLeft}</div>
-
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
+          ))}
+        </div>
+      ) : (
+        <div className="row">
+          {exploreItems.map((item, index) => (
+            <div
+              key={index}
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ display: "block", backgroundSize: "cover" }}
+            >
+              <div
+                className="nft__item"
+                data-aos="fade-up"
+                data-aos-duration="1000"
+              >
+                <div className="author_list_pp">
+                  <Link
+                    to={`/author/${item.authorId}`}
+                    data-bs-toggle="tooltip"
+                    data-bs-placement="top"
+                  >
+                    <img className="lazy" src={item.authorImage} alt="" />
+                    <i className="fa fa-check"></i>
+                  </Link>
+                </div>
+                <div className="de_countdown">{item.timeLeft}</div>
+                <div className="nft__item_wrap">
+                  <div className="nft__item_extra">
+                    <div className="nft__item_buttons">
+                      <button>Buy Now</button>
+                      <div className="nft__item_share">
+                        <h4>Share</h4>
+                        <a href="" target="_blank" rel="noreferrer">
+                          <i className="fa fa-facebook fa-lg"></i>
+                        </a>
+                        <a href="" target="_blank" rel="noreferrer">
+                          <i className="fa fa-twitter fa-lg"></i>
+                        </a>
+                        <a href="">
+                          <i className="fa fa-envelope fa-lg"></i>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <Link to={`/item-details/${item.nftId}`}>
+                    <img
+                      src={item.nftImage}
+                      className="lazy nft__item_preview"
+                      alt=""
+                    />
+                  </Link>
+                </div>
+                <div className="nft__item_info">
+                  <Link to={`/item-details/${item.nftId}`}>
+                    <h4>{item.title}</h4>
+                  </Link>
+                  <div className="nft__item_price">{item.price} ETH</div>
+                  <div className="nft__item_like">
+                    <i className="fa fa-heart"></i>
+                    <span>{item.likes}</span>
                   </div>
                 </div>
               </div>
-              <Link to={`/item-details/${item.nftId}`}>
-                <img
-                  src={item.nftImage}
-                  className="lazy nft__item_preview"
-                  alt=""
-                />
-              </Link>
             </div>
-            <div className="nft__item_info">
-              <Link to={`/item-details/${item.nftId}`}>
-                <h4>{item.title}</h4>
-              </Link>
-              <div className="nft__item_price">{item.price} ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>{item.likes}</span>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
-      ))}
+      )}
       <div className="col-md-12 text-center">
         <button onClick={loadMore} id="loadmore" className="btn-main lead">
           Load more
--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,27 +1,26 @@
 import React, { useEffect, useState } from "react";
 import EthImage from "../images/ethereum.svg";
 import { Link, useParams } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
-import nftImage from "../images/nftImage.jpg";
+import Skeleton from "../components/UI/Skeleton"
 import axios from "axios";
-
-
 
 const ItemDetails = () => {
   const [item, setItem] = useState(null);
   const { nftId } = useParams();
   const [error, setError] = useState(null);
-  
+
   useEffect(() => {
     axios
-    .get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${nftId}`)
-    .then((response) => {
-      setItem(response.data);
-    })
-    .catch((error) => {
-      setError("Failed to fetch item details.");
-      console.error("Error fetching item")
-    }) ;
+      .get(
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${nftId}`
+      )
+      .then((response) => {
+        setItem(response.data);
+      })
+      .catch((error) => {
+        setError("Failed to fetch item details.");
+        console.error("Error fetching item");
+      });
     window.scrollTo(0, 0);
   }, [nftId]);
 
@@ -30,9 +29,55 @@ const ItemDetails = () => {
   }
 
   if (!item) {
-    return <div>Loading...</div>;
+    // Skeleton placeholders while loading
+    return (
+      <div id="wrapper">
+        <div className="no-bottom no-top" id="content">
+          <div id="top"></div>
+          <section aria-label="section" className="mt90 sm-mt-0">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-6 text-center">
+                  <Skeleton width="100%" height="400px" borderRadius="16px" />
+                </div>
+                <div className="col-md-6">
+                  <div className="item_info">
+                    <Skeleton width="60%" height="32px" borderRadius="8px" />
+                    <div className="item_info_counts d-flex justify-content-between mt-3">
+                      <Skeleton width="30%" height="20px" borderRadius="8px" />
+                      <Skeleton width="30%" height="20px" borderRadius="8px" />
+                    </div>
+                    <Skeleton width="100%" height="80px" borderRadius="8px" className="mt-3" />
+                    <div className="d-flex flex-row mt-4">
+                      <div className="mr40">
+                        <h6>Owner</h6>
+                        <div className="item_author d-flex align-items-center">
+                          <Skeleton width="50px" height="50px" borderRadius="50%" />
+                          <Skeleton width="120px" height="20px" borderRadius="8px" className="ml-3" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="spacer-40"></div>
+                    <h6>Creator</h6>
+                    <div className="item_author d-flex align-items-center">
+                      <Skeleton width="50px" height="50px" borderRadius="50%" />
+                      <Skeleton width="120px" height="20px" borderRadius="8px" className="ml-3" />
+                    </div>
+                    <div className="spacer-40"></div>
+                    <h6>Price</h6>
+                    <div className="nft-item-price d-flex align-items-center">
+                      <Skeleton width="50px" height="50px" borderRadius="50%" />
+                      <Skeleton width="100px" height="30px" borderRadius="8px" className="ml-3" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
   }
-  
 
   return (
     <div id="wrapper">
@@ -62,21 +107,25 @@ const ItemDetails = () => {
                       {item.likes}
                     </div>
                   </div>
-                  <p>
-                    {item.description}
-                  </p>
+                  <p>{item.description}</p>
                   <div className="d-flex flex-row">
                     <div className="mr40">
                       <h6>Owner</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to={`/author/${item.author}`}>
-                            <img className="lazy" src={item.ownerImage} alt="" />
+                          <Link to={`/author/${item.ownerId}`}>
+                            <img
+                              className="lazy"
+                              src={item.ownerImage}
+                              alt=""
+                            />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to={`/author/${item.author}`}>{item.ownerName}</Link>
+                          <Link to={`/author/${item.ownerId}`}>
+                            {item.ownerName}
+                          </Link>
                         </div>
                       </div>
                     </div>
@@ -87,13 +136,19 @@ const ItemDetails = () => {
                       <h6>Creator</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to={`/author/${item.author}`}>
-                            <img className="lazy" src={item.creatorImage} alt="" />
+                          <Link to={`/author/${item.creatorId}`}>
+                            <img
+                              className="lazy"
+                              src={item.creatorImage}
+                              alt=""
+                            />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">{item.creatorName}</Link>
+                          <Link to={`/author/${item.creatorId}`}>
+                            {item.creatorName}
+                          </Link>
                         </div>
                       </div>
                     </div>
